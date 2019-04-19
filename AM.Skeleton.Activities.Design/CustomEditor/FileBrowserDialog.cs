d162f8c304710b8be062dc2866a6609da23477dc
MichaelVictore <michael.victore@anothermonday.com>
2019-04-19 17:23:54 +0200

    Better description for custom editor example

--- AM.Skeleton.Activities.Design/CustomEditor/FileBrowserDialog.cs
+++ AM.Skeleton.Activities.Design/CustomEditor/FileBrowserDialog.cs
@@ -5,22 +5,25 @@ using System.Windows.Forms;
 namespace AM.Skeleton.Activities.Design.CustomEditor
 {
     /// <summary>
-    /// 
+    ///     An example of how to create a custom editor that will be shown in the property panel.
+    ///     In this case, we create an expression text-box with a button.
     /// </summary>
     public class FileBrowserDialogEditor : DialogPropertyValueEditor
     {
         public FileBrowserDialogEditor()
         {
+            // Get the XAML DataTemplate for this control
             InlineEditorTemplate = (DataTemplate) EditorResources.GetResources()["ExampleEditor"];
         }
 
         public override void ShowDialog(PropertyValue propertyValue,
             IInputElement commandSource)
         {
-            // Instead of a Open File dialog you can start a Custom Window
+            // Instead of a Open File dialog you can start a custom window
             OpenFileDialog browse = new OpenFileDialog();
 
             if (browse.ShowDialog() == DialogResult.OK) propertyValue.StringValue = browse.FileName;
+
             browse.Dispose();
         }
     }
