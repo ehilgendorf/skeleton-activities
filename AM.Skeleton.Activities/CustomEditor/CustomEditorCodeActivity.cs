c2ec8e9cb05f6600bdf6961ce36e6a6d6f3948b0
MichaelVictore <michael.victore@anothermonday.com>
2019-04-19 23:26:22 +0200

    Added missing description

--- AM.Skeleton.Activities/CustomEditor/CustomEditorCodeActivity.cs
+++ AM.Skeleton.Activities/CustomEditor/CustomEditorCodeActivity.cs
@@ -3,31 +3,36 @@ using System.Activities.Presentation.PropertyEditing;
 using System.ComponentModel;
 using AM.Common.Activities.BaseActivities;
 using AM.Common.Activities.Design.Editors;
-using AM.Core.ActivityDesignBase.Attributes;
 using AM.Skeleton.Application;
 
 namespace AM.Skeleton.Activities.CustomEditor
 {
+    /// <summary>
+    ///     Example of how to create custom editors for arguments
+    /// </summary>
     public sealed class CustomEditorCodeActivity : AbstractCodeActivity
     {
-        // InArgument's can be displayed with a custom editor to add more functionality. In this case it will show a FileDialog
+        /// <summary>
+        ///     InArgument's can be displayed with a custom editor to add more functionality. In this case it will show a
+        ///     FileDialog
+        /// </summary>
         [Editor(typeof(FileBrowserDialogEditor), typeof(DialogPropertyValueEditor))]
         public InArgument<string> CustomEditor { get; set; }
 
-        // Enums will be displayed as DropdownBox in the options panel    
+        /// <summary>
+        ///     Enums will be displayed as DropdownBox in the options panel
+        /// </summary>
         public ExamplePropertyOptions Options { get; set; }
 
-        [Category("Output")]
-        [DisplayName("Output*")]
-        [RequiredArgument]
-        [VariableSelectionOutputPopup]
-        [Description("The result of your activity will be shown in this activity")]
+        /// <summary>
+        ///     Result of the asynchronously Processed File
+        /// </summary>
         public OutArgument<string> Output { get; set; }
 
-        
         /// <summary>
-        /// If your activity returns a value, derive from <see cref="AbstractCodeActivity{T}"/>
-        /// and return the value from the Execute method.</summary>
+        ///     If your activity returns a value, derive from <see cref="AbstractCodeActivity{T}" />
+        ///     and return the value from the Execute method.
+        /// </summary>
         /// <param name="context"></param>
         protected override void ExecuteActivity(CodeActivityContext context)
         {
